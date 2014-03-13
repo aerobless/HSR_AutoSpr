4711c4d090ac05367c22ab9540a33f773a560219
TheoWinter <theo.winter@gmail.com>
2014-03-13 11:07:21 +0100

    Modelled language containing all words consisting of binary numbers that can be divided by three.

--- Math2i/src/com/theowinter/PumpingLemma/PumpingLemma.java
+++ Math2i/src/com/theowinter/PumpingLemma/PumpingLemma.java
@@ -2,9 +2,72 @@ package com.theowinter.PumpingLemma;
 
 public class PumpingLemma {
 
+	/**
+	 *	http://en.wikipedia.org/wiki/Pumping_lemma
+	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(binaryDividedByThreeLanguage("10101"));
 	}
-
+	
+	public static void pump(){
+		
+	}
+	
+	public static boolean binaryDividedByThreeLanguage(String input){
+		boolean isPartOfLanguage;
+		char[] charArray = input.toCharArray();
+		int currentStatus = 0;
+		for(char check : charArray){
+			if(currentStatus == 0){
+				currentStatus = status0(Integer.valueOf(check));
+			}
+			else if(currentStatus == 1){
+				currentStatus = status1(Integer.valueOf(check));
+			}
+			else if(currentStatus == 2){
+				currentStatus = status2(Integer.valueOf(check));
+			}
+		}
+		if(currentStatus==0){
+			isPartOfLanguage = true;
+		}
+		else{
+			isPartOfLanguage = false;
+		}
+		return isPartOfLanguage;
+	}
+	
+	private static int status0(int input){
+		int nextState;
+		if(input == 1){
+			nextState = 1;
+		}
+		else{
+			nextState = 0;
+		}
+		return nextState;
+	}
+	
+	private static int status1(int input){
+		int nextState;
+		if(input == 1){
+			nextState = 0;
+		}
+		else{
+			nextState = 2;
+		}
+		return nextState;
+	}
+	
+	private static int status2(int input){
+		int nextState;
+		if(input == 1){
+			nextState = 2;
+		}
+		else{
+			nextState = 1;
+		}
+		return nextState;
+	}
 }
